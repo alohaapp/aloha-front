656e3cbaa8b1f66e59f4706194732d1621cb6d42
David <david.vazquez.alonso@gmail.com>
2018-11-01 19:11:00 +0100

    Added a React Router example

---
+++ src/Router/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import BasicExample from './basicExample';
+
+
+class Router extends Component {
+  render() {
+    return (
+      < BasicExample />
+    )
+  }
+}
+
+export default Router;
