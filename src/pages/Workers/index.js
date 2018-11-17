28bd630dd2e1feed75da23e0678ca45a9c6ccf7b
David Vázquez <david.vazquez.alonso@gmail.com>
2018-11-17 17:45:53 +0100

    create-main-layout Add main layout

--- src/pages/Workers/index.js
+++ src/pages/Workers/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import useAPI from "../../hooks/useAPI";
 import WorkerList from "./WorkerList";
+import Layout from "../Layout";
 import "./Workers.scss";
 
 function Workers(props) {
   const workers = useAPI({ url: "/Worker" });
 
-  return workers ? <WorkerList workers={workers} /> : <h2>Loading...</h2>;
+  const content = workers ? (
+    <WorkerList workers={workers} />
+  ) : (
+    <h2>Loading...</h2>
+  );
+
+  const sidebar = <div>SIDEBAR</div>;
+
+  return <Layout sidebar={sidebar} content={content} />;
 }
 
 export default Workers;
