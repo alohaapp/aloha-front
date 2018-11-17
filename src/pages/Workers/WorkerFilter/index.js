0c40834444e8c52dec8e80a568a576d5899b1e03
David Vázquez <david.vazquez.alonso@gmail.com>
2018-11-17 19:53:55 +0100

    Add search bar

--- src/pages/Workers/WorkerFilter/index.js
+++ src/pages/Workers/WorkerFilter/index.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Field, Label, Control, Input, Icon, Help } from "bloomer";
 import "./WorkerFilter.scss";
 
 function WorkerFilter(props) {
+  const [wordsSearch, setWordsSearch] = useState("");
+
+  const onChangeInput = () => {
+    console.log("aaa");
+  };
+
   return (
     <Field>
-      <Control hasIcons>
-        <Input type="Search" placeholder="Search" />
-      </Control>
+      <Input type="Search" placeholder="Search" onChange={onChangeInput} />
     </Field>
   );
 }
