524597cd85ccdbc8043799feee1a2a628838bc0e
Félix Horro Pita <felix@corunet.com>
2018-11-17 19:45:02 +0100

    Save user & rol in sessionStorage

--- src/components/LoginErrorBoundary/index.js
+++ src/components/LoginErrorBoundary/index.js
@@ -2,25 +2,51 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Login from "./Login";
 
+// TODO: after a real API login, we don't need sessionStorage anymore
+const sessionUser = {
+  set: user => window.sessionStorage.setItem("aloha", JSON.stringify(user)),
+  get: () => JSON.parse(window.sessionStorage.getItem("aloha"))
+};
+
 class LoginErrorBoundary extends Component {
-  state = {
-    loggedIn: false
+  // TODO: after a real API login, we can remove constructor and add:
+  // state = { user: null }
+  constructor(props) {
+    super(props);
+    let user;
+    try {
+      user = sessionUser.get();
+    } catch (e) {
+      user = null;
+    }
+    this.state = { user };
+  }
+
+  setUser = ({ name, rol }) => {
+    this.setState(
+      {
+        user: { name, rol }
+      },
+      () => sessionUser.set(this.state.user)
+    );
   };
 
   componentDidCatch(error) {
     if (error.message === "401") {
-      this.setState({ loggedIn: false });
+      this.setState({ user: null });
     } else {
-      debugger;
       throw error;
     }
   }
 
   render() {
-    if (this.state.loggedIn) {
-      return this.props.children;
+    const { user } = this.state;
+    if (user) {
+      return React.cloneElement(React.Children.only(this.props.children), {
+        user
+      });
     } else {
-      return <Login onLogin={() => this.setState({ loggedIn: true })} />;
+      return <Login onLogin={this.setUser} />;
     }
   }
 }
