ab5136367b33a5530ab7516394cddec5d63a9ce4
Félix Horro Pita <felix@corunet.com>
2018-11-17 17:43:04 +0100

    useAPI allow to override fetch options

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -1,22 +1,32 @@
 import { useEffect, useState } from "react";
 import { API_URL } from "../constants";
 
-export default function({ method = "GET", url }) {
+export default function({ options, url }) {
   const [result, setResult] = useState();
+  const [error, setError] = useState();
+
+  const defaultOptions = {
+    method: "GET"
+  };
 
   useEffect(
     async () => {
       const r = await fetch(API_URL + url, {
-        method
+        ...defaultOptions,
+        ...options
       });
       if (r.ok) {
         setResult(await r.json());
       } else {
-        throw new Error(r.status);
+        setError(r.status);
       }
     },
-    [method, url]
+    [options, url]
   );
+
+  if (error) {
+    throw new Error(error);
+  }
 
   return result;
 }
