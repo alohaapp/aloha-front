618a27a338bbebd3e01f68f5188535e7fde7f364
Nirro <nireiorange@gmx.com>
2018-11-17 19:42:04 +0100

    Map: Restores test code.

--- src/pages/Map/test.js
+++ src/pages/Map/test.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Map from "./";
 
-xit("renders without crashing", () => {
+it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Map />, div);
   ReactDOM.unmountComponentAtNode(div);
