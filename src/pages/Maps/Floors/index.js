3bf409adc88c5c4b8be564f7cf59bfffe2a238f7
Félix Horro Pita <felix@corunet.com>
2018-11-17 12:35:55 +0100

    POC floors list

---
+++ src/pages/Maps/Floors/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import useAPI from "../../../hooks/useAPI";
+
+function Floors(props) {
+  const floors = useAPI({ url: "/floors" });
+  return floors ? (
+    <ul>
+      {floors.map(floor => (
+        <li>{floor.name}</li>
+      ))}
+    </ul>
+  ) : (
+    <h2>Loading...</h2>
+  );
+}
+
+export default Floors;
