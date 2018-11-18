6c90ec27dc2a1d1222987b0fe6ede6d1357b01cf
Nirro <nireiorange@gmx.com>
2018-11-18 13:37:37 +0100

    Map: First rendering commit ?

--- src/pages/Map/MapPanel/index.js
+++ src/pages/Map/MapPanel/index.js
@@ -1,8 +1,25 @@
 import React from "react";
+import MapPanelFilterToolbar from "../MapPanelFilterToolbar";
 import MapPanelImage from "../MapPanelImage";
+import MapPanelWorkstation from "../MapPanelWorkstation";
 
-function MapPanel() {
-  return <MapPanelImage />;
+function MapPanel({ floor }) {
+  if (!floor) return null;
+
+  const filteredWorkstations = floor.workstations;
+
+  return (
+    <div className="Map-panel">
+      <MapPanelFilterToolbar />
+      <div className="Map-panel-display-area">
+        <MapPanelImage src={floor.imageUrl} />
+        {filteredWorkstations &&
+          filteredWorkstations.map(workstation => (
+            <MapPanelWorkstation workstation={workstation} />
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default MapPanel;
