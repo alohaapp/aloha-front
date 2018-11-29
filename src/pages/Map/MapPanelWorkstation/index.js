27491439d4a42799222ca9093c0dc3af74dc02cf
Alejandro López Sánchez <alopez@corunet.com>
2018-11-29 22:39:30 +0100

    workstation: show worker photo when assigned

--- src/pages/Map/MapPanelWorkstation/index.js
+++ src/pages/Map/MapPanelWorkstation/index.js
@@ -1,11 +1,21 @@
 import "./MapPanelWorkstation.scss";
 
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Button } from "bloomer";
+import CRUDContext from "../../../components/CRUDContext";
 import PopupSelectUser from "../PopupSelectUser";
 
 function MapPanelWorkstation({ workstation }) {
   const [isPopupOpened, setIsPopupOpened] = useState(false);
+  const { workersCRUD } = useContext(CRUDContext);
+
+  // Get the worker if the worksation has one
+  let assignedWorker = null;
+  if (workstation.workerId) {
+    assignedWorker = workersCRUD.store.find(
+      worker => worker.id === workstation.workerId
+    );
+  }
 
   const positionStyle = {
     left: `${workstation.x}%`,
@@ -22,13 +32,22 @@ function MapPanelWorkstation({ workstation }) {
 
   return (
     <div>
-      <Button
-        onClick={() => togglePopup()}
-        className="MapPanelWorkstation"
-        style={positionStyle}
-      >
-        <i className="material-icons md-18">person_add</i>
-      </Button>
+      {assignedWorker ? (
+        <img
+          className="assigned-worker-photo"
+          src={assignedWorker.photoUrl}
+          alt="user"
+          style={positionStyle}
+        />
+      ) : (
+        <Button
+          onClick={() => togglePopup()}
+          className="MapPanelWorkstation"
+          style={positionStyle}
+        >
+          <i className="material-icons md-18">person_add</i>
+        </Button>
+      )}
 
       {isPopupOpened ? (
         <PopupSelectUser close={closePopup} workstation={workstation} />
