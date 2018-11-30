0988c9408aa4bea5d2915b6113807e6e8ce56502
Alejandro López Sánchez <alopez@corunet.com>
2018-11-30 20:25:39 +0100

    worker form: add labels to form

--- src/pages/Workers/WorkerForm/index.js
+++ src/pages/Workers/WorkerForm/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ErrorMessage, Field, Form } from "formik";
-import { Button } from "bloomer";
+import { Button, Label } from "bloomer";
 
 export default function(props) {
   const ErrorMessageComponent = ({ errorMessage }) => (
@@ -9,6 +9,7 @@ export default function(props) {
   return (
     <Form>
       <div className="field">
+        <Label>Username</Label>
         <Field
           className="input"
           type="text"
@@ -23,6 +24,7 @@ export default function(props) {
         />
       </div>
       <div className="field">
+        <Label>Name</Label>
         <Field className="input" type="text" name="name" placeholder="Name" />
         <ErrorMessage
           name="name"
@@ -32,6 +34,7 @@ export default function(props) {
         />
       </div>
       <div className="field">
+        <Label>Surname</Label>
         <Field
           className="input"
           type="text"
@@ -46,6 +49,7 @@ export default function(props) {
         />
       </div>
       <div className="field">
+        <Label>Email</Label>
         <Field
           className="input"
           type="email"
@@ -60,6 +64,7 @@ export default function(props) {
         />
       </div>
       <div className="field">
+        <Label>Description</Label>
         <Field
           className="input"
           component="textarea"
