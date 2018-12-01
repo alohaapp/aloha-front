deb9bcd8e979e89d077afdfd07baae464c987733
Alfonso Parga <alfonsoparher@gmail.com>
2018-12-01 17:19:42 +0100

    Not found error and workstation position problem

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -2,19 +2,43 @@ import "./NotFound.scss";
 
 import React, { Component } from "react";
 import LaCosa from "../../assets/img/lacosa.gif";
+import { Link } from "react-router-dom";
+import logoCorunet from "../../assets/logos/logo-corunet.svg";
+import logo from "../../assets/logos/logo.svg";
+import { Columns, Column } from "bloomer";
+import UserBadge from "../Layout/UserBadge";
 
 class NotFound extends Component {
   render() {
     return (
-      <div className="NotFound">
-        <h2>
-          Page not found...{" "}
-          <span role="img" aria-label="404">
-            😞
-          </span>
-        </h2>
-        <img src={LaCosa} alt="404" />
-      </div>
+      <>
+        <Columns className="header">
+          <Column className="logo-company">
+            <Link to="/">
+              <img src={logoCorunet} alt="logo-corunet" />
+            </Link>
+          </Column>
+          <Column className="logo-app" isSize="2/4">
+            <img src={logo} alt="logo" />
+          </Column>
+          <Column className="user-link" isSize="2/4">
+            <UserBadge />
+          </Column>
+        </Columns>
+        <Columns>
+          <Column>
+            <div className="NotFound">
+              <h2>
+                Page not found...{" "}
+                <span role="img" aria-label="404">
+                  😞
+                </span>
+              </h2>
+              <img src={LaCosa} alt="404" />
+            </div>
+          </Column>
+        </Columns>
+      </>
     );
   }
 }
