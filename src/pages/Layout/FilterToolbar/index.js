02a29d27a6c39417ab61f0a0191696e85bd09c8a
Nirei <nireiorange@gmail.com>
2018-12-01 14:26:15 +0100

    Implements the filter.

--- src/pages/Layout/FilterToolbar/index.js
+++ src/pages/Layout/FilterToolbar/index.js
@@ -1,9 +1,25 @@
 import "./FilterToolbar.scss";
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { API_URL } from "../../../constants";
+import FilterContext from "../../../components/FilterContext";
+import Autocomplete from "../../../components/Autocomplete";
+import FallbackImage from "../../../assets/img/fallback.jpg";
 
-const FilterToolbar = ({ isMap, filters = {} }) => {
-  const [searchText, setSearchText] = useState(filters.search);
+const FilterToolbar = ({ isMap }) => {
+  const { workers, workersSearch, search, username } = useContext(
+    FilterContext
+  );
+  const [searchText, setSearchText] = useState(search);
+  const [usernameText, setUsernameText] = useState(username);
+  useEffect(
+    () => {
+      setSearchText(search);
+      setUsernameText(username);
+    },
+    [search, username]
+  );
+
   return (
     <div className="filter-toolbar">
       <div className="filter-text">
@@ -14,9 +30,41 @@ const FilterToolbar = ({ isMap, filters = {} }) => {
             setSearchText(event.target.value);
           }}
         />
-        <Link to={`?${searchText}`}>
+        <Link to={`?search=${searchText}`}>
           <i className="material-icons">search</i>
         </Link>
+      </div>
+      <div className="filter-username">
+        <Autocomplete
+          value={usernameText}
+          onChange={event => {
+            setUsernameText(event.target.value);
+          }}
+        >
+          {(workersSearch &&
+            workersSearch
+              .filter(worker =>
+                worker.userName
+                  .toLowerCase()
+                  .includes(usernameText.toLowerCase())
+              )
+              .map(worker => (
+                <div className="filter-username-dropdown-item">
+                  <Link to={{ search: `username=${worker.userName}` }}>
+                    <img
+                      alt="user"
+                      src={
+                        worker.imageId
+                          ? `${API_URL}/files/${worker.imageId}`
+                          : FallbackImage
+                      }
+                    />
+                    {worker.userName}
+                  </Link>
+                </div>
+              ))) ||
+            []}
+        </Autocomplete>
       </div>
       <div className="filter-switch">
         <Link className={`map-link${isMap ? " active" : ""}`} to={`/map`} />
