9530ce70043c6b407ca2bd84bf1b2701f803b03c
David Vázquez <david.vazquez.alonso@gmail.com>
2018-12-01 15:33:11 +0100

    Change map test

--- src/pages/Map/test.js
+++ src/pages/Map/test.js
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom";
 import { StaticRouter } from "react-router-dom";
 import Map from "./";
 import { CRUDProvider } from "../../components/CRUDContext";
+import { MessageProvider } from "../../components/MessageContext";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
-    <CRUDProvider>
-      <StaticRouter initialEntries={["/map/1"]} context={{}}>
-        <Map />
-      </StaticRouter>
-    </CRUDProvider>,
+    <MessageProvider>
+      <CRUDProvider>
+        <StaticRouter initialEntries={["/map/1"]} context={{}}>
+          <Map />
+        </StaticRouter>
+      </CRUDProvider>
+    </MessageProvider>,
+
     div
   );
   ReactDOM.unmountComponentAtNode(div);
