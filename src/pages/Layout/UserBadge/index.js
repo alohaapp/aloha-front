4d28ee60dc3f6ee1c467661ea55d9bf352f98693
Alejandro López Sánchez <alopez@corunet.com>
2018-12-01 02:53:13 +0100

    user badge: add real user image support

--- src/pages/Layout/UserBadge/index.js
+++ src/pages/Layout/UserBadge/index.js
@@ -1,6 +1,7 @@
 import "./UserBadge.scss";
 
 import React from "react";
+import { API_URL } from "../../../constants";
 import FallbackImage from "../../../assets/img/fallback.jpg";
 
 export default function UserBadge(props) {
@@ -21,7 +22,12 @@ export default function UserBadge(props) {
         <span className="user-badge-info__username">@{user.userName}</span>
       </div>
       <div className="user-badge-image">
-        <img src={FallbackImage} alt="user" />
+        <img
+          src={
+            user.imageId ? `${API_URL}/files/${user.imageId}` : FallbackImage
+          }
+          alt="user"
+        />
       </div>
     </div>
   );
